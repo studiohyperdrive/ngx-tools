d7c222a2f3a9c475b39e9e2e713df33b5ba50d9f
Axelle Vanden Eynde <axelle.vandeneynde@studiohyperdrive.be>
2021-02-24 16:28:26 +0100

    feat: initial setup

---
+++ projects/utils/src/lib/utils.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-utils',
+  template: `
+    <p>
+      utils works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UtilsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
