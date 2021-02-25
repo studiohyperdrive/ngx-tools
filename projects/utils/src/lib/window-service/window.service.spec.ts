862f4b4b1db956006b2a2d5274be605db1a8a669
Axelle Vanden Eynde <axelle.vandeneynde@studiohyperdrive.be>
2021-02-25 10:09:48 +0100

    fix: lint

--- projects/utils/src/lib/window-service/window.service.spec.ts
+++ projects/utils/src/lib/window-service/window.service.spec.ts
@@ -6,90 +6,90 @@ import { WindowService } from './window.service';
 import { windowMock } from './window.service.mock';
 
 describe('WindowService', () => {
-    describe('with no document', () => {
-        let service: WindowService;
+	describe('with no document', () => {
+		let service: WindowService;
 
-        beforeEach(async () => {
-            await TestBed.configureTestingModule({
-                providers: [{
-                    provide: DOCUMENT,
-                    useValue: null,
-                }, {
-                    provide: PLATFORM_ID,
-                    useValue: 'server',
-                },
-                    WindowService,
-                ],
-            }).compileComponents();
+		beforeEach(async () => {
+			await TestBed.configureTestingModule({
+				providers: [{
+					provide: DOCUMENT,
+					useValue: null,
+				}, {
+					provide: PLATFORM_ID,
+					useValue: 'server',
+				},
+					WindowService,
+				],
+			}).compileComponents();
 
-            service = TestBed.inject(WindowService);
-        });
+			service = TestBed.inject(WindowService);
+		});
 
-        describe('construct', () => {
-            it('should set the width$ BehaviourSubject to a default value of 1200 for SSR', () => {
-                expect(service.width$.getValue()).toBe(1200);
-            });
-        });
+		describe('construct', () => {
+			it('should set the width$ BehaviourSubject to a default value of 1200 for SSR', () => {
+				expect(service.width$.getValue()).toBe(1200);
+			});
+		});
 
-        describe('hasDocument', () => {
-            it('should return false', () => {
-                expect(service.hasDocument()).toBeFalse();
-            });
-        });
+		describe('hasDocument', () => {
+			it('should return false', () => {
+				expect(service.hasDocument()).toBeFalse();
+			});
+		});
 
-        describe('isBrowser', () => {
-            it('should return false', () => {
-                expect(service.isBrowser()).toBeFalse();
-            });
-        });
-    });
+		describe('isBrowser', () => {
+			it('should return false', () => {
+				expect(service.isBrowser()).toBeFalse();
+			});
+		});
+	});
 
-    describe('with a document', () => {
-        let service: WindowService;
+	describe('with a document', () => {
+		let service: WindowService;
 
-        beforeEach(async () => {
-            await TestBed.configureTestingModule({
-                providers: [{
-                    provide: DOCUMENT,
-                    useValue: windowMock,
-                }, {
-                    provide: PLATFORM_ID,
-                    useValue: 'browser',
-                },
-                    WindowService,
-                ],
-            }).compileComponents();
+		beforeEach(async () => {
+			await TestBed.configureTestingModule({
+				providers: [{
+					provide: DOCUMENT,
+					useValue: windowMock,
+				}, {
+					provide: PLATFORM_ID,
+					useValue: 'browser',
+				},
+					WindowService,
+				],
+			}).compileComponents();
 
-            service = TestBed.inject(WindowService);
-        });
+			service = TestBed.inject(WindowService);
+		});
 
-        describe('construct', () => {
-            it('should set the width$ BehaviourSubject to the value of the window-width', () => {
-                expect(service.width$.getValue()).toBe(windowMock.defaultView.innerWidth);
-            });
-        });
+		describe('construct', () => {
+			it('should set the width$ BehaviourSubject to the value of the window-width', () => {
+				expect(service.width$.getValue()).toBe(windowMock.defaultView.innerWidth);
+			});
+		});
 
-        describe('scrollTo', () => {
-            it('should use the window.scrollTo to move to a position on the page', () => {
-                // tslint:disable-next-line: no-string-literal
-                const spy = spyOn(service['window'], 'scrollTo');
+		describe('scrollTo', () => {
+			it('should use the window.scrollTo to move to a position on the page', () => {
+				// tslint:disable-next-line: no-string-literal
+				const spy = spyOn(service['window'], 'scrollTo');
 
-                service.scrollTo(200);
+				service.scrollTo(200);
 
-                expect(spy).toHaveBeenCalledWith(0, 200);
-            });
-        });
+				expect(spy).toHaveBeenCalledWith(0, 200);
+			});
+		});
 
-        describe('hasDocument', () => {
-            it('should return true', () => {
-                expect(service.hasDocument()).toBeTrue();
-            });
-        });
+		describe('hasDocument', () => {
+			it('should return true', () => {
+				expect(service.hasDocument()).toBeTrue();
+			});
+		});
 
-        describe('isBrowser', () => {
-            it('should return true', () => {
-                expect(service.isBrowser()).toBeTrue();
-            });
-        });
-    });
+		describe('isBrowser', () => {
+			it('should return true', () => {
+				expect(service.isBrowser()).toBeTrue();
+			});
+		});
+	});
 });
--- projects/utils/src/lib/window-service/window.service.ts
+++ projects/utils/src/lib/window-service/window.service.ts
@@ -4,43 +4,43 @@ import { BehaviorSubject } from 'rxjs';
 
 // @dynamic
 @Injectable({
-    providedIn: 'root',
+	providedIn: 'root',
 })
 export class WindowService {
-    public width$: BehaviorSubject<number> = new BehaviorSubject<number>(1200);
+	public width$: BehaviorSubject<number> = new BehaviorSubject<number>(1200);
 
-    private window: Window;
+	private window: Window;
 
-    constructor(
-        @Inject(DOCUMENT) public document: Document,
-        // tslint:disable-next-line: ban-types
-        @Inject(PLATFORM_ID) private platformId: string
-    ) {
-        if (this.isBrowser() && this.hasDocument()) {
-            this.window = this.document.defaultView;
+	constructor(
+		@Inject(DOCUMENT) public document: Document,
+		// tslint:disable-next-line: ban-types
+		@Inject(PLATFORM_ID) private platformId: string
+	) {
+		if (this.isBrowser() && this.hasDocument()) {
+			this.window = this.document.defaultView;
 
-            this.width$.next(this.window.innerWidth);
+			this.width$.next(this.window.innerWidth);
 
-            this.window.addEventListener('resize', () => {
-                if (this.window.innerWidth && this.width$.getValue()) {
-                    this.width$.next(this.window.innerWidth);
-                }
-            });
-        }
-    }
+			this.window.addEventListener('resize', () => {
+				if (this.window.innerWidth && this.width$.getValue()) {
+					this.width$.next(this.window.innerWidth);
+				}
+			});
+		}
+	}
 
-    public scrollTo(offset: number = 0): void {
-        if (!this.window) {
-            return;
-        }
-        this.window.scrollTo(0, offset);
-    }
+	public scrollTo(offset: number = 0): void {
+		if (!this.window) {
+			return;
+		}
+		this.window.scrollTo(0, offset);
+	}
 
-    public hasDocument(): boolean {
-        return !!this.document;
-    }
+	public hasDocument(): boolean {
+		return !!this.document;
+	}
 
-    public isBrowser(): boolean {
-        return isPlatformBrowser(this.platformId);
-    }
+	public isBrowser(): boolean {
+		return isPlatformBrowser(this.platformId);
+	}
 }
