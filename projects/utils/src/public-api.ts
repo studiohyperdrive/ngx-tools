3a3d145a40d40494b5875c63b0315f41db1cde1d
Denis Valcke <denis.valcke@studiohyperdrive.be>
2021-03-16 16:13:59 +0100

    fix: expose window-service mocks for testing purposes

--- projects/utils/src/public-api.ts
+++ projects/utils/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of utils
  */
 export { WindowService } from './lib/window-service/window.service';
+export { windowMock, windowServiceMock } from './lib/window-service/window.service.mock';
+
 export { SubscriptionService } from './lib/subscription-service/subscription.service';
 export * from './lib/utils.module';
 
