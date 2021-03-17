cb3498abfe7704be2d995c8dd78e2325d7d4596d
Denis Valcke <denis.valcke@studiohyperdrive.be>
2021-03-17 08:47:26 +0100

    fix: remaining jasmine in window-service mocks

--- projects/utils/src/lib/window-service/window.service.mock.ts
+++ projects/utils/src/lib/window-service/window.service.mock.ts
@@ -1,13 +1,20 @@
 import { BehaviorSubject } from 'rxjs';
 
-export const windowMock = {
-	addEventListener: jasmine.createSpy(),
+/*
+* This windowMock constant return an object with a selected spy.
+* The reason why the spy is not provided by default is for this mock
+* to work on both Jest and Jasmine based test-suites.
+*
+* example:
+* windowMock(jasmine.createSpy());
+* */
+export const windowMock = (spy: unknown) => ({
+	addEventListener: spy,
 	defaultView: {
 		scrollTo: () => null,
-		addEventListener: () => null,
 		innerWidth: 720,
 	},
-};
+});
 
 export const windowServiceMock = (width: number = 1200) => ({
 	width: new BehaviorSubject(width),
