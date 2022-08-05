e5d28363e410a78371f7f9d5ca9616220bde53df
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2022-08-05 10:08:44 +0200

    feat(forms): Add depended required validator - ngx-forms

--- projects/forms/src/lib/validators/email/extended-email.validator.spec.ts
+++ projects/forms/src/lib/validators/email/extended-email.validator.spec.ts
@@ -1,28 +1,28 @@
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
-import { Validators } from '../validators';
+import { NgxValidators } from '../validators';
 
 describe('Extended Email Validator', () => {
 	it('should throw an error if e-mail is not valid', () => {
 		const control1 = new UntypedFormControl('test');
 		const control2 = new UntypedFormControl('test@test');
 
-		expect(Validators.extendedEmail(control1)).toEqual({ extendedEmail: true });
-		expect(Validators.extendedEmail(control2)).toEqual({ extendedEmail: true });
+		expect(NgxValidators.extendedEmail(control1)).toEqual({ extendedEmail: true });
+		expect(NgxValidators.extendedEmail(control2)).toEqual({ extendedEmail: true });
 	});
 
 	it('should not throw an error if e-mail is valid', () => {
 		const control1 = new UntypedFormControl(''); // Should be valid, use Validators.required for this
 		const control2 = new UntypedFormControl('test@test.be');
 
-		expect(Validators.extendedEmail(control1)).toBeNull();
-		expect(Validators.extendedEmail(control2)).toBeNull();
+		expect(NgxValidators.extendedEmail(control1)).toBeNull();
+		expect(NgxValidators.extendedEmail(control2)).toBeNull();
 	});
 
 	it('should work as validator in a reactive form', () => {
 		const form = new UntypedFormGroup({
-			email1: new UntypedFormControl('', [Validators.extendedEmail]),
-			email2: new UntypedFormControl('', [Validators.extendedEmail]),
+			email1: new UntypedFormControl('', [NgxValidators.extendedEmail]),
+			email2: new UntypedFormControl('', [NgxValidators.extendedEmail]),
 		});
 
 		form.get('email1').setValue('test@test');
