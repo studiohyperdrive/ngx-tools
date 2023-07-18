72fbcd527762122c949e31766d642547f2e57b06
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2023-07-18 14:09:54 +0200

    feat(at-least-one-required-validator): Adds an at-least-one-required validator (#15)

---
+++ projects/forms/src/lib/validators/at-least-one-required/at-least-one-required.validator.spec.ts
@@ -0,0 +1,82 @@
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { atLeastOneRequiredValidator } from './at-least-one-required.validator';
+
+describe('atLeastOneRequiredValidator', () => {
+	let form: FormGroup;
+	let formWithKeys: FormGroup;
+	let formWithKeysAndMatchFunction: FormGroup;
+	beforeEach(() => {
+		form = new FormGroup(
+			{
+				a: new FormControl(''),
+				b: new FormControl(''),
+				c: new FormControl(''),
+			},
+			atLeastOneRequiredValidator()
+		);
+
+		formWithKeys = new FormGroup(
+			{
+				a: new FormControl(''),
+				b: new FormControl(''),
+				c: new FormControl(''),
+			},
+			atLeastOneRequiredValidator({controls: ['b', 'c']})
+		);
+
+		formWithKeysAndMatchFunction = new FormGroup(
+			{
+				a: new FormControl(''),
+				b: new FormControl(''),
+				c: new FormControl(''),
+			},
+			atLeastOneRequiredValidator({controls: ['b', 'c'], conditionalFunction: (value) => value.a})
+		);
+	});
+
+	it('should not give an error', () => {
+		form.patchValue({
+			a: true,
+		});
+		expect(form.valid).toEqual(true);
+
+		formWithKeys.patchValue({
+			b: true,
+		});
+		expect(formWithKeys.valid).toEqual(true);
+
+		formWithKeysAndMatchFunction.patchValue({
+			a: true,
+			b: true,
+		});
+		expect(formWithKeysAndMatchFunction.valid).toEqual(true);
+
+		formWithKeysAndMatchFunction.reset();
+		expect(formWithKeysAndMatchFunction.valid).toEqual(true);
+	});
+
+	it('should give an error', () => {
+		expect(form.errors).toEqual({ atLeastOneRequiredError: true });
+		expect(form.get('a').errors).toEqual({ required: true });
+		expect(form.get('c').errors).toEqual({ required: true });
+		expect(form.get('b').errors).toEqual({ required: true });
+
+		expect(formWithKeys.errors).toEqual({ atLeastOneRequiredError: true });
+		expect(formWithKeys.get('c').errors).toEqual({ required: true });
+		expect(formWithKeys.get('b').errors).toEqual({ required: true });
+
+		formWithKeysAndMatchFunction.patchValue({
+			a: true,
+		});
+		expect(formWithKeysAndMatchFunction.errors).toEqual({
+			atLeastOneRequiredError: true,
+		});
+		expect(formWithKeysAndMatchFunction.get('c').errors).toEqual({
+			required: true,
+		});
+		expect(formWithKeysAndMatchFunction.get('b').errors).toEqual({
+			required: true,
+		});
+	});
+});
--- projects/forms/src/lib/validators/validators.ts
+++ projects/forms/src/lib/validators/validators.ts
@@ -1,6 +1,15 @@
-import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+	AbstractControl,
+	FormGroup,
+	ValidationErrors,
+	ValidatorFn,
+} from '@angular/forms';
 
 import { allOrNothingRequiredValidator } from './all-or-nothing-required/all-or-nothing-required.validator';
+import {
+	AtLeastOneRequiredValidatorOptions,
+	atLeastOneRequiredValidator,
+} from './at-least-one-required/at-least-one-required.validator';
 import { extendedEmailValidator } from './email/extended-email.validator';
 
 /**
@@ -26,6 +35,19 @@ export class Validators {
 	 */
 	static allOrNothingRequired(control: FormGroup): ValidationErrors | null {
 		return allOrNothingRequiredValidator(control);
+	}
+
+	/**
+	 * A validator to check if at least one of the provided controls of the form group are filled in
+	 *
+	 * @param options - An optional object with configuration options, see below params for more info
+	 * @param controlNames - Optional list of controls, if not provided the validator is applied to all controls of the group
+	 * @param conditionalFunction - Optional function the form value needs to return true to for the required to be se
+	 */
+	static atLeastOneRequired<KeyType extends string = string>(
+		options?: AtLeastOneRequiredValidatorOptions<KeyType>
+	): ValidatorFn {
+		return atLeastOneRequiredValidator<KeyType>(options);
 	}
 
 	// Add other custom validators :-)
