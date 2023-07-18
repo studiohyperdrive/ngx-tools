bf5840d9586a0baf38519b41eaee6f6b16ede293
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2023-07-18 13:49:50 +0200

    feat(image-loading): Deprecate the image-loading component - ngx-35

--- projects/images/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of images
- */
-
-export * from './lib/images.module';
-export { ProgressiveImageLoadingComponent } from './lib/progressive-image-loading/progressive-image-loading.component';
