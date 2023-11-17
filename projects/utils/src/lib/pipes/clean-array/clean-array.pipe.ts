f18c2a7ae2447b2b35d8e2232d75d0a059eb288c
Wouter Heirstrate <56075460+WHeirstrate@users.noreply.github.com>
2023-11-17 09:27:03 +0100

    build(ngx-utils): v16.4.0
    Co-authored-by: Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>

---
+++ projects/utils/src/lib/pipes/clean-array/clean-array.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'cleanArray',
+})
+export class CleanArrayPipe implements PipeTransform {
+	/**
+	 * Removes all falsy values from the provided array.
+	 *
+	 * @param value The values that need to be stripped of falsy values.
+	 * @param exceptions The falsy values that may be included in the filtered array.
+	 * @returns The filtered array.
+	 */
+	public transform(value: any[], exceptions: any[] = []): any[] {
+		if (!exceptions.length) {
+			return value.filter(Boolean);
+		}
+
+		return value.filter((entry) => Boolean(entry) || exceptions.includes(entry));
+	}
+}
