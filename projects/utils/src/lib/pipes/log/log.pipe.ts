1f19da0f84d2fc88813934dc34b3a50b9a1c9795
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2023-11-17 09:51:12 +0100

    feat(log): add logpipe for debugging

---
+++ projects/utils/src/lib/pipes/log/log.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'log',
+})
+export class LogPipe implements PipeTransform {
+	/**
+	 * Logs the provided value to the console.
+	 *
+	 * @param value The value to log to the console.
+	 * @param text An optional textual value to print before the piped value.
+	 */
+	public transform(value: any, text?: string): void {
+		(text && console.log(text, value)) || console.log(value);
+	}
+}
