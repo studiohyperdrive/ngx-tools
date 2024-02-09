b7d10446b537e038333a974394c30bf642308928
Denis Valcke <denis.valcke@studiohyperdrive.be>
2024-02-09 16:27:13 +0100

    feat(testing): Set up the new package with two initial factories (sidenote: clean up formatting and linting)

--- libs/nodejs/rxjs/src/lib/operators/catch-and-call-through/catch-and-call-through.operator.spec.ts
+++ libs/nodejs/rxjs/src/lib/operators/catch-and-call-through/catch-and-call-through.operator.spec.ts
@@ -20,44 +20,44 @@ describe('catchAndCallThrough', () => {
 			});
 	});
 
-  it('should catch an error, and stop the flow when asked', async () => {
-    throwError({
-      nope: true,
-    })
-      .pipe(
-        catchAndCallThrough((error: unknown) => {
-          expect(error).toEqual({
-            nope: true,
-          });
-        }, 'complete')
-      )
-      .subscribe(() => {
-        // We don't need to test a value, just see if the subscription is triggered.
-        expect(true).toBeTruthy();
-      });
-  });
+	it('should catch an error, and stop the flow when asked', async () => {
+		throwError({
+			nope: true,
+		})
+			.pipe(
+				catchAndCallThrough((error: unknown) => {
+					expect(error).toEqual({
+						nope: true,
+					});
+				}, 'complete')
+			)
+			.subscribe(() => {
+				// We don't need to test a value, just see if the subscription is triggered.
+				expect(true).toBeTruthy();
+			});
+	});
 
-  it('should catch an error, and throw the error again asked', async () => {
-    throwError({
-      nope: true,
-    })
-      .pipe(
-        catchAndCallThrough((error: unknown) => {
-          expect(error).toEqual({
-            nope: true,
-          });
-        }, 'throw'),
-        catchError((error) => {
-          expect(error).toEqual({
-            nope: true,
-          });
+	it('should catch an error, and throw the error again asked', async () => {
+		throwError({
+			nope: true,
+		})
+			.pipe(
+				catchAndCallThrough((error: unknown) => {
+					expect(error).toEqual({
+						nope: true,
+					});
+				}, 'throw'),
+				catchError((error) => {
+					expect(error).toEqual({
+						nope: true,
+					});
 
-          return of(true);
-        })
-      )
-      .subscribe(() => {
-        // We don't need to test a value, just see if the subscription is triggered.
-        expect(true).toBeTruthy();
-      });
-  });
+					return of(true);
+				})
+			)
+			.subscribe(() => {
+				// We don't need to test a value, just see if the subscription is triggered.
+				expect(true).toBeTruthy();
+			});
+	});
 });
