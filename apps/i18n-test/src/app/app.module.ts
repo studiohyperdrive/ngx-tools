061ec33346cb6b8a7aa4d6120e73c992af899a35
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-06-10 07:55:33 +0200

    upgrade(18.0.0): Upgrade to Angular 18.0.0

--- apps/i18n-test/src/app/app.module.ts
+++ apps/i18n-test/src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxI18nModule } from '@ngx/i18n';
 
-@NgModule({
-	declarations: [AppComponent],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		HttpClientModule,
-		NgxI18nModule.forRoot({
-			defaultLanguage: 'nl',
-			availableLanguages: ['nl', 'fr'],
-			defaultAssetPaths: ['./assets/shared/'],
-		}),
-	],
-	providers: [],
-	bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        NgxI18nModule.forRoot({
+            defaultLanguage: 'nl',
+            availableLanguages: ['nl', 'fr'],
+            defaultAssetPaths: ['./assets/shared/'],
+        })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
