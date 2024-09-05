d51dd4ea07b4a2bf7200a825f44e2e69896610bd
Wouter Heirstrate <56075460+WHeirstrate@users.noreply.github.com>
2024-09-05 10:45:22 +0200

    feat(ngx-inform): export the injection token to use the tooltip in non-angular environments (#214)
    Co-authored-by: Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>

--- libs/inform/src/lib/index.ts
+++ libs/inform/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './types';
 export * from './directives';
 export * from './providers';
 export * from './abstracts';
+export * from './tokens';
--- libs/inform/src/lib/tokens/tooltip/tooltip-configuration.token.ts
+++ libs/inform/src/lib/tokens/tooltip/tooltip-configuration.token.ts
@@ -3,7 +3,9 @@ import { InjectionToken } from '@angular/core';
 import { NgxTooltipConfiguration } from '../../types';
 
 /**
- * A token to provide the necessary configuration to the NgxTooltipDirective
+ * A token to provide the necessary configuration to the NgxTooltipDirective. This is exported
+ * due to testing frameworks (like Storybook) not being able to resolve the InjectionToken in the
+ * `provideNgxTooltipConfiguration`.
  */
 export const NgxTooltipConfigurationToken = new InjectionToken<NgxTooltipConfiguration>(
 	'NgxTooltipConfigurationConfiguration'
