c4a4f508c88be2c545d96314e5ebf281c4e729c5
SHDBramVandenbussche <bram.vandenbussche@studiohyperdrive.be>
2024-10-23 09:57:01 +0200

    feat(docs): add ng-docs

--- apps/cookies-test/src/app/app.config.ts
+++ apps/cookies-test/src/app/app.config.ts
@@ -1,7 +1,11 @@
+import { provideNgDocApp, provideSearchEngine, NgDocDefaultSearchEngine, providePageSkeleton, NG_DOC_DEFAULT_PAGE_SKELETON, provideMainPageProcessor, NG_DOC_DEFAULT_PAGE_PROCESSORS } from "@ng-doc/app";
+import { NG_DOC_ROUTING, provideNgDocContext } from "@ng-doc/generated";
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(appRoutes)],
+	providers: [provideRouter(appRoutes), provideAnimations(), provideHttpClient(withInterceptorsFromDi()), provideRouter(NG_DOC_ROUTING, withInMemoryScrolling({scrollPositionRestoration: "enabled", anchorScrolling: "enabled"})), provideHttpClient(withInterceptorsFromDi(), withFetch()), provideNgDocContext(), provideNgDocApp(), provideSearchEngine(NgDocDefaultSearchEngine), providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON), provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS)],
 };
