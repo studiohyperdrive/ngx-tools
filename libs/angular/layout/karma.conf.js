ec94324d384be686696294914cb6117494c66e68
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-12-02 11:10:30 +0100

    feat(global): Move ngx-layout lib

--- libs/angular/layout/karma.conf.js
+++ libs/angular/layout/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 			suppressAll: true, // removes the duplicated traces
 		},
 		coverageReporter: {
-			dir: require('path').join(__dirname, '../../coverage/layout'),
+			dir: require('path').join(__dirname, '../../../coverage/angular/layout'),
 			subdir: '.',
 			reporters: [{ type: 'html' }, { type: 'text-summary' }],
 		},
