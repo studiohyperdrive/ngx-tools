7f388bece56d2db21c0f3d7e434ab174e188b5f3
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-12-02 12:59:24 +0100

    feat(global): Move ngx-i18n lib

--- libs/angular/i18n/karma.conf.js
+++ libs/angular/i18n/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 			suppressAll: true, // removes the duplicated traces
 		},
 		coverageReporter: {
-			dir: require('path').join(__dirname, '../../coverage/i18n'),
+			dir: require('path').join(__dirname, '../../../coverage/i18n'),
 			subdir: '.',
 			reporters: [{ type: 'html' }, { type: 'text-summary' }],
 		},
