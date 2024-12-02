114bd2d4246f302ab6bbeacbe7aac02b1e05fcd5
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-12-02 12:58:43 +0100

    feat(global): Move ngx-forms package

--- libs/angular/forms/karma.conf.js
+++ libs/angular/forms/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 			suppressAll: true, // removes the duplicated traces
 		},
 		coverageReporter: {
-			dir: require('path').join(__dirname, '../../coverage/forms'),
+			dir: require('path').join(__dirname, '../../../coverage/forms'),
 			subdir: '.',
 			reporters: [{ type: 'html' }, { type: 'text-summary' }],
 		},
