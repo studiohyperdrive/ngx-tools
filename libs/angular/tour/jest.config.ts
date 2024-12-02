8b26e78ae470b7fdce2295dd49c6ea08c09dd26b
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-12-02 13:06:18 +0100

    feat(global): Move ngx-tour lib

--- libs/angular/tour/jest.config.ts
+++ libs/angular/tour/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
 	displayName: 'tour',
-	preset: '../../jest.preset.js',
+	preset: '../../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-	coverageDirectory: '../../coverage/libs/tour',
+	coverageDirectory: '../../../coverage/libs/angular/tour',
 	transform: {
 		'^.+\\.(ts|mjs|js|html)$': [
 			'jest-preset-angular',
