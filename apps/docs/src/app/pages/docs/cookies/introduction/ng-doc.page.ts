7f10f1afa7358cbefb11b7148c3588a50f2eed86
SHDBramVandenbussche <113341869+SHDBramVandenbussche@users.noreply.github.com>
2024-12-02 09:27:47 +0100

    feat(cookies-docs): Add cookies docs
    * feat(cookies-docs): Add cookies docs
    
    * feat(i18n-docs): add i18n docs and fix directive names in md files
    
    * feat(i18n-docs): add i18n docs and fix directive names in md files
    
    * feat(inform-tour-docs): Add inform and tour docs

---
+++ apps/docs/src/app/pages/docs/cookies/introduction/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import { CookiesCategory } from '../../../../categories';
+
+const IntroductionPage: NgDocPage = {
+	title: `Introduction`,
+	mdFile: './index.md',
+	category: CookiesCategory,
+	order: 0,
+};
+
+export default IntroductionPage;
