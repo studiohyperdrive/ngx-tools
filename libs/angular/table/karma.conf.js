79ffabda952a4ab93471d3bfb2e54a6852122576
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2024-12-02 13:05:31 +0100

    feat(global): Move ngx-table lib

--- libs/angular/table/karma.conf.js
+++ libs/angular/table/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 			suppressAll: true, // removes the duplicated traces
 		},
 		coverageReporter: {
-			dir: require('path').join(__dirname, '../../coverage/table'),
+			dir: require('path').join(__dirname, '../../../coverage/angular/table'),
 			subdir: '.',
 			reporters: [{ type: 'html' }, { type: 'text-summary' }],
 		},
