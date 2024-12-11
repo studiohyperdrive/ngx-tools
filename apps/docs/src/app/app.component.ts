b600eac46f5f3a4d3af70ad7ec0db680b45b43ac
Ian Emsens <ian.emsens@studiohyperdrive.be>
2024-12-11 13:19:57 +0100

    fix(build): resolve generic build "errors" and apply linting in multiple libs

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from '@ng-doc/app';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-	imports: [RouterModule, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
+	imports: [RouterModule],
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
