88b8d169f795e9fd7398e33d2ddadc919c0b8a91
Wouter <wouter.bastaens@gmail.com>
2024-12-31 14:26:10 +0100

    fix(nx-core): add stylelint

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,23 @@ module.exports = {
 	tabWidth: 4,
 	trailingComma: 'es5',
 	useTabs: true,
+	plugins: ['prettier-plugin-organize-attributes'],
+	attributeGroups: [
+		'$ANGULAR_STRUCTURAL_DIRECTIVE',
+		'^class$',
+		'^(id|name)$',
+		'$DEFAULT',
+		'^aria-',
+		'$ANGULAR_INPUT',
+		'$ANGULAR_TWO_WAY_BINDING',
+		'$ANGULAR_OUTPUT'
+	],
+	overrides: [
+		{
+			files: ['*.html'],
+			options: {
+				parser: 'angular'
+			}
+		}
+	]
 };
