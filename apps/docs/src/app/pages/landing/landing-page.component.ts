74cad805ff11d29bafdafe23d1336c9805ef972e
Wouter <wouter.bastaens@gmail.com>
2025-01-06 14:27:50 +0100

    feat(docs): some animation on landing page

--- apps/docs/src/app/pages/landing/landing-page.component.ts
+++ apps/docs/src/app/pages/landing/landing-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { ERoutes } from '../../shared/types';
 
 @Component({
@@ -7,7 +8,38 @@ import { ERoutes } from '../../shared/types';
 	selector: 'landing-page',
 	templateUrl: 'landing-page.component.html',
 	styleUrl: 'landing-page.component.scss',
+	animations: [
+		trigger('slideIn', [
+			state(
+				'open',
+				style({
+					width: '100%',
+				})
+			),
+			state(
+				'closed',
+				style({
+					width: '0%',
+				})
+			),
+			transition('open => closed', [animate('1s ease-in-out')]),
+		]),
+	],
 })
 export class LandingPageComponent {
 	public routes: typeof ERoutes = ERoutes;
+
+	isCovered = true;
+
+	@ViewChild('quote', { static: false }) private quote: ElementRef<HTMLDivElement> | undefined;
+
+	@HostListener('window:scroll', ['$event'])
+	isScrolledIntoView() {
+		if (this.quote && this.isCovered) {
+			const rect = this.quote.nativeElement.getBoundingClientRect();
+			const topShown = rect.top >= 0;
+			const bottomShown = rect.bottom <= window.innerHeight;
+			this.isCovered = !(topShown && bottomShown);
+		}
+	}
 }
