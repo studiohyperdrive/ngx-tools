0945eba703f34851f732dc34cf4b0f2629a19b55
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-01-09 13:58:25 +0100

    chore: added some more documentation inline

--- libs/javascript/utils/src/lib/object/search-recursively/search-recursively.util.ts
+++ libs/javascript/utils/src/lib/object/search-recursively/search-recursively.util.ts
@@ -8,17 +8,25 @@ export const searchRecursively = <ItemType extends { children: ItemType[] }>(
 	list: ItemType[],
 	callbackFn: (item: ItemType) => boolean
 ): ItemType | undefined => {
+	// Femke: The final result if we find any
 	let result: ItemType | undefined;
+
+	// Femke: Loop over the current list
 	list.forEach((item) => {
+		// Femke: If we have already a result, no need to search further
 		if (result) {
 			return;
 		}
 
+		// Femke: Check the item if it matches what we are searching for
 		if (callbackFn(item)) {
+			// Femke: Found a match, so store it
 			result = item;
 		} else {
+			// Femke: No match found, search the children of the item
 			result = searchRecursively(item.children, callbackFn);
 		}
 	});
+
 	return result;
 };
