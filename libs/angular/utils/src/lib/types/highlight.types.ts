6bbbefdcde18eb4171efe467352709c73b6fab3f
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-01-16 11:31:15 +0100

    chore: fix TS docs for configuration

--- libs/angular/utils/src/lib/types/highlight.types.ts
+++ libs/angular/utils/src/lib/types/highlight.types.ts
@@ -1,18 +1,31 @@
 /**
- * @property normalized - search for matches should be normalized
- * @property caseInsensitive - search for matches should be caseInsensitive
- * @property splitTextToHighlight - text to highlight should be split on space.
- * If false the entire string will be used for searching e.g. "some value"
- * If true the value will be split e.g. "some" or "value"
- * @property someOrEveryMatch - highlight only the first match or all matches
- * @property tag - tag used to highlight
- * @property highlightClass - class used to highlight
+ * The configuration used to highlight determine what and how to highlight
  */
 export interface NgxHighlightConfiguration {
+	/**
+	 * Whether the search for matches in the provided string should be normalized.
+	 */
 	normalized?: boolean;
+	/**
+	 * Whether the search for matches in the provided string should be case-insensitive.
+	 */
 	caseInsensitive?: boolean;
+	/**
+	 * Should only the first match be highlighted or all matches
+	 */
 	someOrEveryMatch?: 'some' | 'every';
+	/**
+	 * Whether text to highlight should be split on space.
+	 * If false the entire string will be used for searching e.g. "some value"
+	 * If true the value will be split e.g. "some" or "value"
+	 */
 	splitTextToHighlight?: boolean;
+	/**
+	 * Which HTML tag should be used to highlight
+	 */
 	tag?: string;
+	/**
+	 * Which class should be applied to the {@link tag}.
+	 */
 	highlightClass?: string;
 }
